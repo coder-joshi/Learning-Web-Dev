382b77223765cb27595c6f0e842e9490c151ab50
coder-joshi <101914709+coder-joshi@users.noreply.github.com>
2025-06-10 23:52:16 +0530

    Delete WEEK 5 directory

--- WEEK 5/WEEK 5_1/index.js
+++
@@ -1,7 +0,0 @@
-async function getRecentPost(){
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-    const data = await response.json();
-    console.log(data);
-}
-
-getRecentPost   ()

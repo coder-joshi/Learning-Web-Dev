03cb7874fed11a12e64a17c2e756866a4733faa8
coder-joshi <101914709+coder-joshi@users.noreply.github.com>
2025-07-24 00:17:10 +0530

    Delete WEEK 11 directory

--- WEEK 11/Routers/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {handleUserSignUp,handleUserLogin} = require("../Controllers/controller.user")
-
-router.post("/signup",handleUserSignUp);
-router.post("/login",handleUserLogin);   
-
-module.exports = router;
--- WEEK 11/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectMongoDb(url){
-    mongoose.connect(url)
-    .then(()=>{console.log("MongoDB has connected successfully")})
-    .catch((err)=>{console.log("MongoDB ran into error. The error is:",err)});
-}
-
-module.exports = {connectMongoDb}
